86967152642a4924ce8e24f1d9b7c7010b451af7
Fatih Furkan Çambel <cambelfatih01@gmail.com>
2023-08-06 00:09:35 +0300

    search denemesi

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { ParkList, ParkingDetail } from './housinglocation';
+import { ParkList, ParkingDetail} from './housinglocation';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,7 @@ export class HousingService {
 
   url = 'https://api.ibb.gov.tr/ispark/Park';
   urldetay = 'https://localhost:7233/api/Parking'
+  UrlSearch = 'https://localhost:7233/api/Parking/search'
 
   async getAllParkList(): Promise<ParkList[]> {
     const data = await fetch(this.url);
@@ -19,7 +22,6 @@ export class HousingService {
     const data = await fetch(`${this.urldetay}/${id}`);
     return await data.json() ?? {};
   }
-  
 
 }
 
